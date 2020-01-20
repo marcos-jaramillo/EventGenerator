c655c573fd8602e8d3afaf93e3e431effd249968
C.MAJAR@tessa.com.uy <C.MAJAR@tessa.com.uy>
2020-01-20 10:29:47 -0600

    Stateless Conection to kie server

---
+++ EventGenerator/src/main/java/com/ternium/core/eventgenerator/util/KeyGenerator.java
@@ -0,0 +1,15 @@
+package com.ternium.core.eventgenerator.util;
+
+public class KeyGenerator {
+	private static final long LIMIT = 10000000000L;
+	private static long last = 0;
+
+	public static long getKey() {
+	  // 10 digits.
+	  long id = System.currentTimeMillis() % LIMIT;
+	  if ( id <= last ) {
+	    id = (last + 1) % LIMIT;
+	  }
+	  return last = id;
+	}
+}
